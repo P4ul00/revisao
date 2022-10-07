c5558d57eafd26ae583ef2346a3e71881e8ab0ac
Paulo <111285911+P4ul00@users.noreply.github.com>
2022-10-07 11:29:01 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ export default function Home() {
     <Header/>
     <h4>Pagina inicial</h4>
     <h5>testee</h5>
-    <a href="pagina2.html">ir para pagina 2</a>
     </>
   )
 
 
 }
+<a href="pagina2.html">ir para pagina 2</a>
